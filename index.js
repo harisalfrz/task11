dc09e6115babd0d6c2a0578a9ab659ba54e41c46
Raphaelinooooo <michey2809@gmail.com>
2023-08-29 21:26:26 +0700

    first commit

--- index.js
+++ index.js
@@ -7,11 +7,59 @@ app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "src/views"));
 
 // Asset Files
-app.use(express.static('src/views/assets'))
+app.use(express.static('src/assets'))
 app.use(express.urlencoded({ extended: false }));
 
+const contentBlog = [
+  {
+    projectName: "Vue JS",
+    authorName: "Haris Alfariz",
+    postedAt: getFullTime(new Date()),
+    startDate: "2023-06-01",
+    endDate: "2023-08-20",
+    description:
+      "Vue.js adalah framework JavaScript yang bisa digunakan untuk mengembangkan aplikasi. Biasanya, framework ini digunakan untuk dua hal, yaitu membangun User Interface (UI) dan Single Page Application (SPA). Bisa dibilang, Vue.js adalah salah satu framework yang cukup populer.",
+    inputNodejs: "",
+    inputReactjs: "",
+    inputVuejs: "on",
+    inputJavascript: "",
+    inputImg: "/images/vuejs.jpg",
+  },
+  {
+    projectName: "Keyboard Mechanical",
+    authorName: "Haris Alfariz",
+    postedAt: getFullTime(new Date()),
+    startDate: "2023-06-08",
+    endDate: "2023-08-08",
+    description:
+      "  Keyboard mechanical adalah keyboard yang awet, berkualitas, dan nyaman digunakan sehingga cocok untuk bekerja atau bermain game.",
+    inputNodejs: "on",
+    inputReactjs: "on",
+    inputVuejs: "on",
+    inputJavascript: "on",
+    inputImg:
+      "/images/keyboard-mechanical.jpg",
+  },
+  {
+    projectName: "Type Script",
+    authorName: "Haris Alfariz",
+    postedAt: getFullTime(new Date()),
+    startDate: "2022-05-01",
+    endDate: "2023-08-20",
+    description:
+      "TypeScript adalah bahasa pemrograman open source yang dibangun di atas JavaScript.",
+    inputNodejs: "on",
+    inputReactjs: "on",
+    inputVuejs: "",
+    inputJavascript: "on",
+    inputImg:
+      "/images/typescript-icon-512x512-we5ze0xe.png",
+  },
+  
+];
+
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { contentBlog });
 });
 
 app.get("/form-blog", (req, res) => {
@@ -21,24 +69,83 @@ app.get("/form-blog", (req, res) => {
 app.post("/form-blog", (req, res) => {
   const {
     projectName,
+    authorName,
     startDate,
     endDate,
     description,
     inputNodejs,
     inputReactjs,
-    inputNextjs,
-    inputTypescript,
+    inputVuejs,
+    inputJavascript,
   } = req.body;
 
-  console.log("Project Name: ", projectName);
-  console.log("Start Date: ", startDate);
-  console.log("End Date: ", endDate);
-  console.log("Description: ", description);
-  console.log("Node JS: ", inputNodejs);
-  console.log("React JS: ", inputReactjs);
-  console.log("Next JS: ", inputNextjs);
-  console.log("TypeScript: ", inputTypescript);
+  const durasi = getDurasi(startDate, endDate);
 
+  const content = {
+    projectName,
+    authorName,
+    postedAt: getFullTime(new Date()),
+    startDate,
+    endDate,
+    description,
+    inputNodejs,
+    inputReactjs,
+    inputVuejs,
+    inputJavascript,
+    durasi,
+  };
+
+  contentBlog.push(content);
+  content.ids = contentBlog.length - 1;
+  
+  res.redirect("/");
+});
+
+app.get("/update-project/:id", (req, res) => {
+  const id = req.params.id;
+
+  res.render("update-project", { data: contentBlog[id] });
+});
+
+app.post("/update-project/:id", (req, res) => {
+  
+  let editURL = req.originalUrl;
+  console.log(editURL);
+
+  const {
+    ids,
+    projectName,
+    authorName,
+    startDate,
+    endDate,
+    description,
+    inputNodejs,
+    inputReactjs,
+    inputVuejs,
+    inputJavascript,
+    inputImg,
+  } = req.body;
+
+  const durasi = getDurasi(startDate, endDate);
+
+  const content = {
+    ids,
+    projectName,
+    authorName,
+    postedAt: getFullTime(new Date()),
+    startDate,
+    endDate,
+    description,
+    inputNodejs,
+    inputReactjs,
+    inputVuejs,
+    inputJavascript,
+    inputImg,
+    durasi,
+  };
+  console.log(content);
+
+  contentBlog.splice(ids, 1, content);
   res.redirect("/");
 });
 
@@ -46,18 +153,54 @@ app.get("/contact", (req, res) => {
   res.render("contact");
 });
 
-app.get("/blog-detail", (req, res) => {
+app.get("/blog-content/:id", (req, res) => {
   const id = req.params.id;
-  const data = {
-    id,
-    title: "TypeScript",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi beatae doloribus tempora dolore, veritatis laudantium quos quae totam libero hic nesciunt vero tenetur sint voluptas similique enim accusantium. Libero repellendus perspiciatis incidunt ipsum, quae, officia odit laborum ab aut ducimus eligendi deleniti sequi doloremque repellat eos facilis facere aliquid. Incidunt recusandae fugiat quas. Quisquam minus similique tempore, illo beatae rem sapiente? Adipisci rerum fugiat tempora corporis maiores! Architecto delectus perspiciatis quos tempore quisquam. Asperiores nulla repudiandae ad sint distinctio iste libero ipsa assumenda, illo iure rerum a id maxime nisi nemo tempore tempora, quisquam ullam, eveniet fugiat. Doloribus magni earum quasi impedit exercitationem possimus sunt, veritatis accusamus excepturi corrupti unde, omnis ullam enim voluptas est, saepe iste! Corporis consectetur nostrum nulla aut distinctio impedit! Ut perferendis repudiandae architecto culpa repellendus! Minima quia, quo dolor quam reprehenderit sint sit ullam nulla dolorem sequi molestias accusamus excepturi, consequatur dolorum? Qui ipsa distinctio veritatis vero est eaque dolore animi explicabo possimus deserunt, ratione aut delectus, at modi ab illum pariatur sint vitae? Quia, repellendus quo. Velit fugiat dolorum quibusdam eius fugit architecto excepturi officiis pariatur, aut nulla esse ipsum, nisi voluptatum non necessitatibus quas tempore! Laborum eaque perspiciatis et sed autem fugiat commodi?",
-  };
 
-  res.render("blog-detail", { data });
+  res.render("blog-content", { data: contentBlog[id] });
+});
+
+app.get("/delete-blog/:id", (req, res) => {
+  const id = req.params.id;
+
+  contentBlog.splice(id, 1);
+  res.redirect("/");
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+// Functions
+contentBlog.forEach((blog, index) => {
+  blog.durasi = getDurasi(blog.startDate, blog.endDate);
+  blog.ids = index;
+});
+
+function getDurasi(startDate, endDate) {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+  const timeDifference = end - start;
+  const durationInDays = Math.floor(timeDifference / (1000 * 3600 * 24));
+  let daysInMonth = 30;
+  let monthsInYear = 12;
+  let durasiMonth = Math.floor(durationInDays / daysInMonth);
+  let durasiYear = Math.floor(durationInDays / (daysInMonth * monthsInYear));
+
+  if (durasiYear > 0) {
+    return `Durasi: ${durasiYear} tahun`;
+  } else if (durasiMonth > 0) {
+    return `Durasi: ${durasiMonth} bulan`;
+  } else if (durationInDays > 0) {
+    return `Durasi: ${durationInDays} hari`;
+  } else {
+    return "";
+  }
+}
+
+function getFullTime(time) {
+  return new Intl.DateTimeFormat("en-GB", {
+    dateStyle: "long",
+    timeStyle: "short",
+    timeZone: "Asia/Jakarta",
+  }).format(time);
+}
